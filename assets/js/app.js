6d0d9392eb67282a36d1e103a1996f20097fafe4
wmlee <e2goon@gmail.com>
2019-12-06 00:22:48 +0900

    update: 폴더 구성 일괄 변경
    * 메인 상단 메뉴 마크업
    * 보도뉴스 마크업

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,30 @@
 
-var swiperMain = new Swiper('.notify', {
-  pagination: {
-    el: '.notify .swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      var notifyItem = this.slides[index].querySelector('.notify-item');
-      return '<button type="button" class="' + className + '">' + notifyItem.dataset.bulletName + '</button>';
-    },
+(function() {
+
+  var swiperMain = new Swiper('.notify', {
+    loop: true,
+    pagination: {
+      el: '.notify .swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        var notifyItem = this.slides[index].querySelector('.notify-item');
+        return '<button type="button" class="' + className + '">' + notifyItem.dataset.bulletName + '</button>';
+      },
+    }
+  });
+
+  function mainMenu() {
+    var menu = document.querySelector('.main-menu');
+    window.addEventListener('scroll', function() {
+      if (this.scrollY < 50) {
+        menu.classList.remove('is-active');
+      } else {
+        menu.classList.add('is-active');
+      }
+    });
   }
-});
+
+  window.addEventListener('DOMContentLoaded', function() {
+    mainMenu();
+  })
+} ());
